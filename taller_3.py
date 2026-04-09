c5b999d4c6c6645a2bc2e5812f4e5c312e70b75e
Angela Medina <angelamedina03.2016@gmail.com>
2026-04-08 21:23:19 -0500

    Agregar envio

--- taller_3.py
+++ taller_3.py
@@ -9,3 +9,17 @@ class Nodo:
 class ListaDoble:
     def _init_(self):
         self.cabeza = None
+    # AGREGAR ENVÍO
+    def agregar(self, codigo, cliente, estado):
+        nuevo = Nodo(codigo, cliente, estado)
+
+        if self.cabeza is None:
+            self.cabeza = nuevo
+            return
+
+        actual = self.cabeza
+        while actual.siguiente:
+            actual = actual.siguiente
+
+        actual.siguiente = nuevo
+        nuevo.anterior = actual
